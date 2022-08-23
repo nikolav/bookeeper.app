d217676121192e82214f88b7f2fc33a1d104b072
nikolav <admin@nikolav.rs>
2022-08-23 04:36:24 +0100

    + <Popper>

--- src/app/pages/PageHome/PageHome.jsx
+++ src/app/pages/PageHome/PageHome.jsx
@@ -1,7 +1,7 @@
+import { useRef, useState } from "react";
 import Button from "@mui/material/Button";
 // import { toast } from "react-toastify";
 // import { supabase as s } from "../../services";
-import { Collapse } from "../../../components";
 import { useStateSwitch } from "../../../hooks";
 
 //
@@ -13,19 +13,6 @@ export default function PageHome() {
       <Button onClick={toggle} variant="outlined" color="primary" size="small">
         ok
       </Button>
-      <Collapse isOpen={isActive}>
-        <div className="bg-orange-200">
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt qui
-            perspiciatis praesentium odio ab numquam, dignissimos minus nemo non
-            eveniet provident nesciunt optio quia fugiat quae voluptates
-            eligendi corporis pariatur magnam aperiam. Possimus perspiciatis
-            illo, quidem a minima facilis facere quis, quibusdam, eveniet
-            temporibus mollitia dignissimos officia delectus adipisci aliquid
-            earum doloremque voluptas reprehenderit! Adipisci?
-          </p>
-        </div>
-      </Collapse>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere
         nesciunt suscipit perspiciatis.
--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,13 @@
 import { RouteTransition, AnimatedRoutes } from "./AnimatedRoutes";
 import Collapse from "./Collapse/Collapse";
+import Popper from "./Popper/Popper"
 import PortalOverlays from "./PortalOverlays/PortalOverlays";
 import ToastMessages from "./ToastMessages/ToastMessages";
 
 export {
   AnimatedRoutes,
   Collapse,
+  Popper,
   PortalOverlays,
   RouteTransition,
   ToastMessages,
