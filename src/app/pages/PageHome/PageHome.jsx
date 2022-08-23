a76c3d86ebf87fd29e46e14ab48bfaf6999077ad
nikolav <admin@nikolav.rs>
2022-08-23 04:02:59 +0100

    + <Collapse>

--- src/app/pages/PageHome/PageHome.jsx
+++ src/app/pages/PageHome/PageHome.jsx
@@ -1,18 +1,35 @@
 import Button from "@mui/material/Button";
-import { toast } from "react-toastify"
-import { supabase as s } from "../../services"
+// import { toast } from "react-toastify";
+// import { supabase as s } from "../../services";
+import { Collapse } from "../../../components";
+import { useStateSwitch } from "../../../hooks";
 
-// 
+//
 export default function PageHome() {
-    return (
-      <section className="text-center">
-        <h1>@index</h1>
-        <Button onClick={() => toast.info(Date.now())} variant="outlined" color="primary" size="small">ok</Button>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere
-          nesciunt suscipit perspiciatis.
-        </p>
-      </section>
-    );
-  }
-  
+  const { isActive, toggle } = useStateSwitch();
+  return (
+    <section className="text-center">
+      <h1>@index</h1>
+      <Button onClick={toggle} variant="outlined" color="primary" size="small">
+        ok
+      </Button>
+      <Collapse isOpen={isActive}>
+        <div className="bg-orange-200">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt qui
+            perspiciatis praesentium odio ab numquam, dignissimos minus nemo non
+            eveniet provident nesciunt optio quia fugiat quae voluptates
+            eligendi corporis pariatur magnam aperiam. Possimus perspiciatis
+            illo, quidem a minima facilis facere quis, quibusdam, eveniet
+            temporibus mollitia dignissimos officia delectus adipisci aliquid
+            earum doloremque voluptas reprehenderit! Adipisci?
+          </p>
+        </div>
+      </Collapse>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere
+        nesciunt suscipit perspiciatis.
+      </p>
+    </section>
+  );
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,12 @@
 import { RouteTransition, AnimatedRoutes } from "./AnimatedRoutes";
+import Collapse from "./Collapse/Collapse";
 import PortalOverlays from "./PortalOverlays/PortalOverlays";
 import ToastMessages from "./ToastMessages/ToastMessages";
 
-export { AnimatedRoutes, PortalOverlays, RouteTransition, ToastMessages };
+export {
+  AnimatedRoutes,
+  Collapse,
+  PortalOverlays,
+  RouteTransition,
+  ToastMessages,
+};
