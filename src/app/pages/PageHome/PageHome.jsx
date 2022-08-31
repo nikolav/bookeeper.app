86adfcf44517384e1bf1b4915f077406063a4c91
nikolav <admin@nikolav.rs>
2022-08-31 14:15:31 +0100

    inits fs

--- src/app/pages/PageHome/PageHome.jsx
+++ src/app/pages/PageHome/PageHome.jsx
@@ -1,31 +1,61 @@
 import { useState, useRef } from "react";
-// import { Filesystem, ApplicationBar } from "../../../components";
-// import { getNodeKey } from "../../../util";
+import { Filesystem } from "../../../components";
+import { getNodeKey } from "../../../util";
 //
-
 export default function PageHome() {
-  // const [f$, setf] = useState();
+  const [f$, setf] = useState();
   //
   return (
     <section className="text-center">
       <h1>@index</h1>
-      {/* <ApplicationBar /> */}
       <hr className="m-0 p-0 block mb-2" />
-      {/* <pre className="bg-stone-900/50 text-xs min-h-[6rem] text-left flex items-center">
+      <pre className="bg-stone-900/50 text-xs min-h-[6rem] text-left flex items-center">
         {f$ ? JSON.stringify(f$, null, 2) : "No file selected."}
-      </pre> */}
-      {/* <Filesystem
-        ID="grcnxyhffbb"
-        onSelect={(node) => {
-          if (!node) {
-            setf(null);
-            return;
-          }
-          //
-          const { label: file } = node.value();
-          setf({ path: getNodeKey(node), file });
-        }}
-      /> */}
+      </pre>
+      <div className="grid grid-cols-2 gap-2">
+        <div>
+          <Filesystem
+            ID="grcnxyhffbb"
+            onSelect={(node) => {
+              if (!node) {
+                setf(null);
+                return;
+              }
+              //
+              const { label: file } = node.value();
+              setf({ path: getNodeKey(node), file });
+            }}
+          />
+        </div>
+
+        <div>
+          <p>
+            {" "}
+            <em>ReactJS</em> <strong>&lt;Filesystem&gt;</strong>
+          </p>
+          <p>Filesystem component for desktop type online applications.</p>
+          <p>
+            <a
+              className="link text-indigo-500"
+              rel="norefferer noopenenr"
+              target="_blank"
+              // href={GIT_LINK}
+            >
+              code@github
+            </a>
+          </p>
+          <p>
+            <a
+              className="link text-indigo-500"
+              rel="norefferer noopenenr"
+              target="_blank"
+              href="https://nikolav.rs/"
+            >
+              admin
+            </a>
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
