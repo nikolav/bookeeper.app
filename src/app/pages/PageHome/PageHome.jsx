641833a5433498cfa56bf58a268ac5c8d73b765f
nikolav <admin@nikolav.rs>
2022-08-31 14:33:33 +0100

    setup

--- src/app/pages/PageHome/PageHome.jsx
+++ src/app/pages/PageHome/PageHome.jsx
@@ -2,59 +2,54 @@ import { useState, useRef } from "react";
 import { Filesystem } from "../../../components";
 import { getNodeKey } from "../../../util";
 //
+const GIT_LINK =
+  "https://github.com/nikolav/bookeeper.app/blob/production--filesystem/src/components/Filesystem/Filesystem.jsx";
+//
 export default function PageHome() {
   const [f$, setf] = useState();
   //
   return (
     <section className="text-center">
       <h1>@index</h1>
-      <hr className="m-0 p-0 block mb-2" />
-      <pre className="bg-stone-900/50 text-xs min-h-[6rem] text-left flex items-center">
+      <hr className="m-0 p-0 block" />
+      <pre className="bg-stone-900/50 text-xs min-h-[6rem] text-left flex items-center rounded p-2">
         {f$ ? JSON.stringify(f$, null, 2) : "No file selected."}
       </pre>
-      <div className="grid grid-cols-2 gap-2">
-        <div>
-          <Filesystem
-            ID="grcnxyhffbb"
-            onSelect={(node) => {
-              if (!node) {
-                setf(null);
-                return;
-              }
-              //
-              const { label: file } = node.value();
-              setf({ path: getNodeKey(node), file });
-            }}
-          />
-        </div>
-
-        <div>
-          <p>
-            {" "}
-            <em>ReactJS</em> <strong>&lt;Filesystem&gt;</strong>
-          </p>
-          <p>Filesystem component for desktop type online applications.</p>
-          <p>
-            <a
-              className="link text-indigo-500"
-              rel="norefferer noopenenr"
-              target="_blank"
-              // href={GIT_LINK}
-            >
-              code@github
-            </a>
-          </p>
-          <p>
-            <a
-              className="link text-indigo-500"
-              rel="norefferer noopenenr"
-              target="_blank"
-              href="https://nikolav.rs/"
-            >
-              admin
-            </a>
-          </p>
-        </div>
+      <p>
+        <strong>&lt;Filesystem&gt;</strong> komponenta za web aplikacije desktop
+        tipa.
+      </p>
+      <p className="flex justify-evenly items-center">
+        <a
+          className="link text-indigo-500"
+          rel="norefferer noopenenr"
+          target="_blank"
+          href={GIT_LINK}
+        >
+          code@github
+        </a>
+        <a
+          className="link text-indigo-500"
+          rel="norefferer noopenenr"
+          target="_blank"
+          href="https://nikolav.rs/"
+        >
+          admin
+        </a>
+      </p>
+      <div className="px-12 mt-4">
+        <Filesystem
+          ID="grcnxyhffbb"
+          onSelect={(node) => {
+            if (!node) {
+              setf(null);
+              return;
+            }
+            //
+            const { label: file } = node.value();
+            setf({ path: getNodeKey(node), file });
+          }}
+        />
       </div>
     </section>
   );
