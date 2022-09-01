8097235694bbb451e01e6d13388def73ca85086d
nikolav <admin@nikolav.rs>
2022-09-01 23:49:44 +0100

    prod.contextmenu

--- src/app/pages/PageHome/PageHome.jsx
+++ src/app/pages/PageHome/PageHome.jsx
@@ -1,40 +1,52 @@
-import { useState, useRef } from "react";
-import {
-  // Filesystem,
-  // ApplicationBar,
-  ContextMenu,
-} from "../../../components";
+import { useRef } from "react";
+import { ContextMenu } from "../../../components";
 import contextMenu from "../../../assets/context-menu/demo";
-// import { getNodeKey } from "../../../util";
+import imageToolips from "../../../../public/tulips.jpg";
 //
-
+const GIT_LINK =
+  "https://github.com/nikolav/bookeeper.app/blob/production--application-command-bar/src/app/pages/PageHome/PageHome.jsx";
+//
 export default function PageHome() {
   const r$ = useRef();
   //
   return (
     <section className="text-center">
       <h1>@index</h1>
-      {/* <ApplicationBar /> */}
-      <hr className="m-0 p-0 block mb-2" />
-      <button ref={r$} className="button">
-        ok
-      </button>
+      <div className="mt-12">
+        <p className="flex justify-center gap-20">
+          <a
+            className="link text-indigo-500"
+            rel="norefferer noopenenr"
+            target="_blank"
+            href={GIT_LINK}
+          >
+            code@github
+          </a>
+          <a
+            className="link text-indigo-500"
+            rel="norefferer noopenenr"
+            target="_blank"
+            href="https://nikolav.rs/"
+          >
+            admin
+          </a>
+        </p>
+        <p>
+          Komponenta <strong>&lt;ContextMenu&gt;</strong> za web aplikacije
+          desktop tipa.
+        </p>
+      </div>
+
+      <img
+        ref={r$}
+        className="block p-2 rounded border-2 shadow w-[320px] mx-auto mt-4"
+        src={imageToolips}
+        alt="right click to open context menu"
+      />
       <ContextMenu ID="wonnzyhpqpf" menu={contextMenu} anchor={r$} />
-      {/* <pre className="bg-stone-900/50 text-xs min-h-[6rem] text-left flex items-center">
-        {f$ ? JSON.stringify(f$, null, 2) : "No file selected."}
-      </pre> */}
-      {/* <Filesystem
-        ID="grcnxyhffbb"
-        onSelect={(node) => {
-          if (!node) {
-            setf(null);
-            return;
-          }
-          //
-          const { label: file } = node.value();
-          setf({ path: getNodeKey(node), file });
-        }}
-      /> */}
+      <p className="text-center opacity-60 italic">
+        Right Click image to open contextmenu
+      </p>
     </section>
   );
 }
