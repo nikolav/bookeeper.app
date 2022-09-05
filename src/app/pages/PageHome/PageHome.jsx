9ad8c7534843b3c248160f54894ae835be59747e
nikolav <admin@nikolav.rs>
2022-09-05 17:06:21 +0100

    refactor#2

--- src/app/pages/PageHome/PageHome.jsx
+++ src/app/pages/PageHome/PageHome.jsx
@@ -1,15 +1,18 @@
-// import {
-//   // BoxResizeLeft,
-//   // BoxResizeRight,
-//   // BoxResizeTop,
-//   // Spotlight,
-// TabsIndicator,
-// } from "../../../components";
+import {
+  //   // BoxResizeLeft,
+  //   // BoxResizeRight,
+  //   // BoxResizeTop,
+  //   // Spotlight,
+  TabsIndicator,
+} from "../../../components";
+import { useState } from "react";
+import { sample } from "../../../util";
 //
 const GIT_LINK =
   "https://github.com/nikolav/bookeeper.app/blob/production--application-command-bar/src/app/pages/PageHome/PageHome.jsx";
 //
 export default function PageHome() {
+  const [k, setk] = useState("$0");
   //
   return (
     <section className="text-center">
@@ -36,6 +39,10 @@ export default function PageHome() {
           Komponenta <strong>&lt;ContextMenu&gt;</strong> za web aplikacije
           desktop tipa.
         </p>
+      </div>
+      <button onClick={() => setk(sample(["$0", "$1", "$2"]))}>go</button>
+      <div className="**w-64 ***bg-blue-300 mx-auto">
+        <TabsIndicator current={k} />
       </div>
     </section>
   );
--- src/components/TabsIndicator/TabsIndicator.jsx
+++ src/components/TabsIndicator/TabsIndicator.jsx
@@ -8,13 +8,12 @@ const TabsIndicator = ({
   //
   // [
   // { key: string.unique, stop: 0, color: css-color }, 1..
-  // { key: string.unique, stop: 100, color: css-color }, ..N
+  // { key: string.unique, stop: 1, color: css-color }, ..N
   //   ]
   stops = [
     { key: "$0", stop: 0, color: "red" },
-    { key: "$1", stop: 25, color: "green" },
-    { key: "$2", stop: 50, color: "blue" },
-    { key: "$3", stop: 75, color: "orange" },
+    { key: "$1", stop: 1 / 3, color: "green" },
+    { key: "$2", stop: 2 / 3, color: "blue" },
   ],
   //
   current = null,
@@ -30,18 +29,14 @@ const TabsIndicator = ({
   const [k$, setk] = useState(0);
   const to = stops[k$];
   //
-  const x = W * (to.stop / 100);
-  const width = (((stops[k$ + 1]?.stop || 100) - to.stop) / 100) * W;
+  const x = W * to.stop;
+  const width = ((stops[k$ + 1]?.stop || 1) - to.stop) * W;
   const fill = to.color;
   //
   useEffect(() => setk(findIndex(stops, { key: current })), [current]);
   //
   return (
-    <svg
-      viewBox={`0 0 ${W} ${H}`}
-      width="100%"
-      className={`${modcss.canvas} ${className}`}
-    >
+    <svg viewBox={`0 0 ${W} ${H}`} className={`${modcss.canvas} ${className}`}>
       <motion.rect
         stroke="0"
         y="0"
